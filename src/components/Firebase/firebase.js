4de11f83048ae306188e5c64074439ade3b40a04
Vitor Quadros <viitorquadros@hotmail.com>
2022-12-26 13:39:01 -0300

    final

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,60 +1,72 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 import {
-    getAuth
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut
 } from 'firebase/auth';
-import {
-    getDatabase
-} from "firebase/database"
+import { getDatabase, ref, set } from 'firebase/database';
 // import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 class Firebase {
+  constructor() {
+    this.app = initializeApp(firebaseConfig);
+    this.db = getDatabase(this.app);
+    this.auth = getAuth();
+    this.isLogged = false;
+    this.credentials = null;
+  }
 
-    constructor() {
-        this.app = initializeApp(firebaseConfig)
-        this.db = getDatabase(this.app)
-        this.auth = getAuth();
-        this.isLogged = false;
-        this.credentials =null;
+  // *** AUTH API ***
+  async doCreateUserWithEmailAndPassword(email, password) {
+    try {
+      await createUserWithEmailAndPassword(this.auth, email, password);
+
+      await set(ref(this.db, 'users/' + this.auth.currentUser.uid), {
+        email: email,
+        admin: true
+      });
+    } catch (error) {
+      console.error(error.message);
+      throw error;
     }
+  }
 
-    // *** AUTH API ***
-    async doCreateUserWithEmailAndPassword(email, password) {
-        try {
-           //implemente aqui a função para criar um usuário
-           throw new Error("Função indisponível") //remova essa linha
-        } catch (error) {
-            console.error(error.message)
-            throw error;
-        }
+  async doSignInWithEmailAndPassword(email, password) {
+    try {
+      this.credentials = await signInWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+
+      this.credentials ? (this.isLogged = true) : (this.isLogged = false);
+      return this.credentials.user ?? null;
+    } catch (error) {
+      console.error(error.message);
+      throw error;
     }
+  }
 
-    async doSignInWithEmailAndPassword(email, password) {
-        try {
-            /**
-            Quando o usuário estiver logado atribua o valor TRUE
-            ao atributo this.isLogged e as credenciais ao atributo this.credentials
-            deverá retornar a propriedade user do atributo this.credentials.user
-            */
-           //implemente aqui função logar o usuario
-           throw new Error("Função Indisponível!!") //remova essa linha
-        } catch (error) {
-            console.error(error.message)
-            throw error;
-        }
+  doSignOut = async () => {
+    try {
+      await signOut(this.auth);
+      this.isLogged = false;
+      console.log('deslogado');
+    } catch (e) {
+      console.error(e);
     }
-
-    doSignOut = () => {
-    };
+  };
 }
 
 export default Firebase;
